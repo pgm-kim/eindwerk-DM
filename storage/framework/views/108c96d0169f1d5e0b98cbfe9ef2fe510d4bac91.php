fa5d1c82de9b696c3151636463179f08ed3e8365
kimberly <92147193+pgm-kim@users.noreply.github.com>
2022-11-03 20:44:02 +0100

    WTF

---
+++ storage/framework/views/108c96d0169f1d5e0b98cbfe9ef2fe510d4bac91.php
@@ -0,0 +1,38 @@
+<?php $__env->startSection('content'); ?>
+    <body >
+    <main>
+<section>
+    <h2>welcome in Grey's universe, name user </h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. <a class="text-link" href="">Grey's anatomy</a>Tempore autem est fugiat! Assumenda quibusdam sequi tempora harum nemo quasi saepe fugit iure odit dolore dignissimos at neque, qui omnis impedit.<a class="text-link" href="">station 19</a>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.<a class="text-link" href="">private practice</a> Ex qui aspernatur rem, voluptatibus ab ipsam iusto officia nostrum, nobis explicabo laudantium cupiditate id reprehenderit? Voluptatum non assumenda architecto cumque consequatur.
+    </p>
+</section>
+<div>
+                <h2>latest episodes</h2>
+                <div class="accent2"></div>
+            </div>
+    <section class="episodes">
+            <div class="episode">
+                <img src="https://cdn1.edgedatg.com/aws/v2/abc/Station19/video/30440209/b2bfc9c85c68b1078dbd4af30fccd4bf/579x325-Q100_b2bfc9c85c68b1078dbd4af30fccd4bf.jpg" alt="episode image" class="episode_image">
+                <div class="episode_info">
+                    
+                    <h4 class="episode_code">Seizoen 17, episode 1</h4>
+                    <h3>new album taylor swift</h3>
+                    <p>After a year of uncertainty, the doctors of Grey Sloan Memorial are faced with life-or-death decisions that will alter the course of their relationships forever.</p>
+                </div>
+            </div>
+            <div class="episode">
+                <img src="https://cdn1.edgedatg.com/aws/v2/abc/Station19/video/30440209/b2bfc9c85c68b1078dbd4af30fccd4bf/579x325-Q100_b2bfc9c85c68b1078dbd4af30fccd4bf.jpg" alt="episode image" class="episode_image">
+                <div class="episode_info">
+                    
+                    <h4 class="episode_code">Seizoen 17, episode 1</h4>
+                    <h3>new album taylor swift</h3>
+                    <p>After a year of uncertainty, the doctors of Grey Sloan Memorial are faced with life-or-death decisions that will alter the course of their relationships forever.</p>
+                </div>
+            </div>
+</section>
+    </body>
+ <?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\kimsb\Downloads\DM 2022-2023\eindwerk\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c5f53ce26ec22844728b68b7f1b5fafc2cffa29a.php
@@ -0,0 +1,39 @@
+
+<?php $__env->startSection('content'); ?>
+<body>
+    <main>
+        <img src="" alt="">
+        <section><h2>name serie</h2>
+        <div class="accent2"></div>
+        <p>Lorem ipsum dolor sit amet consectetu+r adipisicing elit. Voluptatibus sit rerum doloribus praesentium! Facere fugit accusantium animi, est nemo dolorem ea ipsa architecto a earum beatae, dolore veniam ducimus odit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, vitae, cupiditate consequuntur, dolorum obcaecati quam laudantium cumque eius harum commodi dicta at maiores placeat? Molestias laudantium ratione in a veritatis.</p>
+        </section>
+        <h2>meet the cast</h2>
+        <div class="accent2"></div>
+        <section class="cast">
+        <div class="personage">
+            <img src="https://images.squarespace-cdn.com/content/v1/5d4c0b758a9ffd000154599d/1592986709256-6ZW03NVGZR9GHXDQQPIW/Avatar+for+Testimonials+Page+300.png" alt="ben waren" class="personage_img">
+            <h4>name actor</h4>
+            <h3>name personage</h3>
+        </div>
+        <div class="personage">
+            <img src="https://images.squarespace-cdn.com/content/v1/5d4c0b758a9ffd000154599d/1592986709256-6ZW03NVGZR9GHXDQQPIW/Avatar+for+Testimonials+Page+300.png" alt="ben waren" class="personage_img">
+            <h4>name actor</h4>
+            <h3>name personage</h3>
+        </div>
+        <div class="personage">
+            <img src="https://images.squarespace-cdn.com/content/v1/5d4c0b758a9ffd000154599d/1592986709256-6ZW03NVGZR9GHXDQQPIW/Avatar+for+Testimonials+Page+300.png" alt="ben waren" class="personage_img">
+            <h4>name actor</h4>
+            <h3>name personage</h3>
+        </div>
+        <div class="personage">
+            <img src="https://images.squarespace-cdn.com/content/v1/5d4c0b758a9ffd000154599d/1592986709256-6ZW03NVGZR9GHXDQQPIW/Avatar+for+Testimonials+Page+300.png" alt="ben waren" class="personage_img">
+            <h4>name actor</h4>
+            <h3>name personage</h3>
+            test test
+        </div>
+        
+        </section>
+    </main>
+</body>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\kimsb\Downloads\DM 2022-2023\eindwerk\resources\views/serie.blade.php ENDPATH**/ ?>
